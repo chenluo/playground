a0ed463f99b1ca5086baee0867833e1a61ba908c
chenluo <chenluo.cn@gmail.com>
2022-02-04 00:27:28 +0800

    memory barrier: update link

--- jvm-test/src/main/c/test.c
+++ jvm-test/src/main/c/test.c
@@ -1,5 +1,6 @@
 // gcc -S -masm=intel test.c produces test.s
 // -O2 seems not necessary
+// link: https://preshing.com/20120625/memory-ordering-at-compile-time/
 void test(int i) {
     int j = 0;
     i = 0;
