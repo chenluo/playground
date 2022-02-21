fd8594c40ecbd5a98aa60faffd30ad04380b9626
chenluo <chenluo.cn@gmail.com>
2022-02-21 08:56:55 +0800

    gc init

---
+++ jvm-test/src/main/java/com/chenluo/jvm/learn/gc/TestGcLog.java
@@ -0,0 +1,10 @@
+package com.chenluo.jvm.learn.gc;
+
+public class TestGcLog {
+    public static void main(String[] args) throws InterruptedException {
+        while (true) {
+            byte[] arr = new byte[1024*1024];
+            Thread.sleep(100);
+        }
+    }
+}
