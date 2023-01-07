da70d0664f290fff080d0ca6ad755c274a74efae
chenluo <chenluo.cn@gmail.com>
2023-01-08 01:00:22 +0800

    dockerfile

--- service-foo/src/main/java/com/chenluo/servicefoo/controller/FooController.java
+++ service-foo/src/main/java/com/chenluo/servicefoo/controller/FooController.java
@@ -1,17 +1,16 @@
 package com.chenluo.servicefoo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FooController {
-    @Autowired
-    private ServletWebServerApplicationContext webServerAppCtxt;
+    @Value("${server.port}")
+    private int port;
 
     @GetMapping("greeting")
     public String greeting() {
-        return String.format("hello from %s", webServerAppCtxt.getWebServer().getPort());
+        return String.format("hello from %s", port);
     }
 }
