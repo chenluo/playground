51429856719326a039adc4605fab334c59e20203
chenluo <chenluo.cn@gmail.com>
2023-03-17 00:10:47 +0800

    redis project

--- mw-redis/build.gradle.kts
+++ mw-redis/build.gradle.kts
@@ -1,24 +1,20 @@
 plugins {
     id("java")
-    id("org.springframework.boot") version "3.0.1"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
 }
 
 group = "com.chenluo"
+version = "unspecified"
 
 repositories {
     mavenCentral()
 }
 
-configurations.all() {
-    exclude("org.springframework.boot", "spring-boot-starter-logging")
-}
-
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
     implementation(project(":tool-logger"))
+    implementation("org.redisson:redisson:3.20.0")
+
 }
 
 tasks.getByName<Test>("test") {
---
+++ mw-redis/src/main/java/com/chenluo/RedisTester.java
@@ -0,0 +1,33 @@
+package com.chenluo;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.redisson.Redisson;
+import org.redisson.api.RBitSet;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+
+public class RedisTester {
+    private final Logger logger = LogManager.getLogger(RedisTester.class);
+
+    public static void main(String[] args) {
+        new RedisTester().run();
+    }
+
+    private void run() {
+
+        Config config = new Config();
+        // use "rediss://" for SSL connection
+        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+        RedissonClient client = Redisson.create(config);
+        RBitSet bitset = client.getBitSet("bitset");
+        int bitIndex = 10;
+        logger.info(bitset.get(bitIndex));
+        bitset.set(bitIndex);
+        logger.info(bitset.get(bitIndex));
+        bitset.clear(bitIndex);
+        logger.info(bitset.get(bitIndex));
+        bitset.set(bitIndex);
+        client.shutdown();
+    }
+}
