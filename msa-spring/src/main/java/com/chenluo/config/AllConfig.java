af04b5f6a9de591185c4969a0a0ccd15201c700c
chenluo <chenluo.cn@gmail.com>
2023-06-20 00:24:39 +0800

    spring kafka retry

--- msa-spring/src/main/java/com/chenluo/config/AllConfig.java
+++ msa-spring/src/main/java/com/chenluo/config/AllConfig.java
@@ -1,9 +1,13 @@
 package com.chenluo.config;
 
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.util.backoff.BackOff;
+import org.springframework.util.backoff.FixedBackOff;
 
 @Configuration
 @EnableWebSecurity
@@ -17,4 +21,14 @@ public class AllConfig {
     //                    .anyRequest().authenticated();
     //        });
     //    }
+    @Bean
+    public DefaultErrorHandler errorHandler() {
+        BackOff fixedBackOff = new FixedBackOff(1000, 10);
+        DefaultErrorHandler errorHandler = new DefaultErrorHandler((consumerRecord, e) -> {
+            // logic to execute when all the retry attemps are exhausted
+        }, fixedBackOff);
+        //        errorHandler.addRetryableExceptions(SocketTimeoutException.class);
+        //        errorHandler.addNotRetryableExceptions(NullPointerException.class);
+        return errorHandler;
+    }
 }
--- msa-spring/src/main/java/com/chenluo/kafka/springkafka/Consumers.java
+++ msa-spring/src/main/java/com/chenluo/kafka/springkafka/Consumers.java
@@ -1,12 +1,53 @@
 package com.chenluo.kafka.springkafka;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.kafka.retrytopic.TopicSuffixingStrategy;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Component;
+
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Component
 public class Consumers {
-    @KafkaListener(topics = "test-topic", groupId = "spring-kafka", concurrency = "3")
-    public void consume(String message) {
-        System.out.println(Thread.currentThread().getName() + " : " + message);
+    private final Random random = new Random();
+    private final AtomicInteger i = new AtomicInteger();
+
+    @RetryableTopic(attempts = "2", topicSuffixingStrategy =
+            TopicSuffixingStrategy.SUFFIX_WITH_INDEX_VALUE, backoff = @Backoff(delay = 1000,
+            multiplier = 2.0), autoCreateTopics = "false"
+            //            , exclude = {Exception.class,}
+    )
+    @KafkaListener(topics = "test-topic", groupId = "spring-kafka-nb", concurrency = "1")
+    public void consumeNonBlockRetry(ConsumerRecord<String, String> record, Acknowledgment ack) {
+        System.out.println(Thread.currentThread().getName() + " : " + record.value());
+        if (i.incrementAndGet() % 10 == -1) {
+            ack.acknowledge();
+        } else {
+            throw new RuntimeException(record.value());
+        }
+    }
+
+    @KafkaListener(topics = "test-topic", groupId = "spring-kafka-b", concurrency = "1")
+    public void consumeBlockRetry(ConsumerRecord<String, String> record, Acknowledgment ack) {
+        System.out.println(Thread.currentThread().getName() + " : " + record.value());
+        if (i.incrementAndGet() % 10 == -1) {
+            ack.acknowledge();
+        } else {
+            throw new RuntimeException(record.value());
+        }
+    }
+
+    @DltHandler
+    public void dlt(String message, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
+                    @Header(KafkaHeaders.ORIGINAL_TOPIC) String originTopic) {
+        System.out.println("dlt %s, %s".formatted(message, topic));
+
     }
 }
