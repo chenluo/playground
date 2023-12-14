bd457e2ed54d445f177e524158d6ea1cd5d17a86
chenluo <chenluo.cn@gmail.com>
2023-12-15 00:46:15 +0800

    [java] try virtual thread.

---
+++ lang-jvm/src/main/java/com/chenluo/jvm/learn/virtualthread/VirtualThreadTry.java
@@ -0,0 +1,40 @@
+package com.chenluo.jvm.learn.virtualthread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class VirtualThreadTry {
+    public static void main(String[] args) {
+        try (ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor()) {
+            for (int i = 0; i < 1; i++) {
+                int finalI = i;
+                executorService.execute(() -> {
+                    while (true) {
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                            throw new RuntimeException(e);
+                        }
+                        System.out.println(Thread.currentThread());
+                        Thread.dumpStack();
+                        System.out.println("print in vt" + finalI);
+                    }
+                });
+            }
+            executorService.shutdown();
+            boolean b = executorService.awaitTermination(16, TimeUnit.SECONDS);
+            if (!b) {
+                System.out.println("failed to terminate");
+                System.out.println("Forced to shutdown");
+                executorService.shutdownNow();
+            }
+
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        }
+    }
+
+}
