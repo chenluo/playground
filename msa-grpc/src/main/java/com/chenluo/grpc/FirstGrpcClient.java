9624dc05ad5d365e156c063c1d4d255eb0342564
chenluo <chenluo.cn@gmail.com>
2023-12-15 00:43:39 +0800

    [grpc] stream request and response

--- msa-grpc/src/main/java/com/chenluo/grpc/FirstGrpcClient.java
+++ msa-grpc/src/main/java/com/chenluo/grpc/FirstGrpcClient.java
@@ -3,17 +3,74 @@ package com.chenluo.grpc;
 import io.grpc.Grpc;
 import io.grpc.InsecureChannelCredentials;
 import io.grpc.ManagedChannel;
+import io.grpc.stub.ClientCallStreamObserver;
+import io.grpc.stub.ClientResponseObserver;
+
+import java.util.concurrent.CountDownLatch;
 
 public class FirstGrpcClient {
     public static void main(String[] args) {
-        new FirstGrpcClient().callApi1();
+        FirstGrpcClient client = new FirstGrpcClient();
+//        client.callApi1();
+//        client.callApi2();
+        client.callApi4();
     }
 
     public void callApi1() {
-        Req.Builder req = Req.newBuilder().setId(1).setReq("req1");
         ManagedChannel channel = Grpc.newChannelBuilderForAddress("localhost", 18081, InsecureChannelCredentials.create()).build();
         FirstGrpcServiceGrpc.FirstGrpcServiceBlockingStub blockingStub = FirstGrpcServiceGrpc.newBlockingStub(channel);
-        Res res = blockingStub.api2(Req.newBuilder().setId(1).setReq("req1").build());
+        Res res = blockingStub.api1(Req.newBuilder().setId(1).setReq("req1").build());
         System.out.println(res);
     }
+
+    public void callApi2() {
+        ManagedChannel channel = Grpc.newChannelBuilderForAddress("localhost", 18081, InsecureChannelCredentials.create()).build();
+        FirstGrpcServiceGrpc.FirstGrpcServiceBlockingStub blockingStub = FirstGrpcServiceGrpc.newBlockingStub(channel);
+        blockingStub.api2(Req.newBuilder().setId(2).setReq("req").build()).forEachRemaining(
+                System.out::println
+        );
+    }
+
+    public void callApi4() {
+        ManagedChannel channel = Grpc.newChannelBuilderForAddress("localhost", 18081, InsecureChannelCredentials.create()).build();
+        FirstGrpcServiceGrpc.FirstGrpcServiceStub stub = FirstGrpcServiceGrpc.newStub(channel);
+        final int count = 100;
+        CountDownLatch latch = new CountDownLatch(1);
+        stub.api4(new ClientResponseObserver<Req, Res>() {
+            @Override
+            public void onNext(Res value) {
+                System.out.println(value);
+
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                System.out.println(t);
+                latch.countDown();
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("client complete");
+                latch.countDown();
+            }
+
+            @Override
+            public void beforeStart(ClientCallStreamObserver<Req> requestStream) {
+                requestStream.setOnReadyHandler(() -> {
+                    for (int i = 0; i < count; i++) {
+                        requestStream.onNext(Req.newBuilder().setId(i).setReq("req" + i).build());
+                    }
+                    requestStream.onCompleted();
+                });
+            }
+        });
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("completed");
+    }
+
 }
--- msa-grpc/src/main/java/com/chenluo/grpc/FirstGrpcServiceImpl.java
+++ msa-grpc/src/main/java/com/chenluo/grpc/FirstGrpcServiceImpl.java
@@ -5,11 +5,39 @@ import io.grpc.stub.StreamObserver;
 public class FirstGrpcServiceImpl extends FirstGrpcServiceGrpc.FirstGrpcServiceImplBase {
 
     @Override
-    public void api2(Req request, StreamObserver<Res> responseObserver) {
+    public void api1(Req request, StreamObserver<Res> responseObserver) {
         System.out.println("response");
         System.out.println(request);
         Res res = Res.newBuilder().setId(request.getId()).setReq(request.getReq()).setRes("res:" + request.getReq()).build();
         responseObserver.onNext(res);
         responseObserver.onCompleted();
     }
+
+    @Override
+    public void api2(Req request, StreamObserver<Res> responseObserver) {
+        for (int i = 0; i < 10; i++) {
+            responseObserver.onNext(Res.newBuilder().setId(request.getId()).setReq(request.getReq()).setRes(i + ":" + request.getReq()).build());
+        }
+    }
+
+    @Override
+    public StreamObserver<Req> api4(StreamObserver<Res> responseObserver) {
+        return new StreamObserver<Req>() {
+            @Override
+            public void onNext(Req value) {
+                responseObserver.onNext(Res.newBuilder().setReq(value.getReq()).setRes(value.getReq() + " res").setId(value.getId()).build());
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                System.out.println(t);
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("completed");
+            }
+        };
+    }
+
 }
