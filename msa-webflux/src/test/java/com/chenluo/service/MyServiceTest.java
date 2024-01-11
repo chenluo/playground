249267cdb22292339933f4acc3f5301097ab61ce
chenluo <chenluo.cn@gmail.com>
2024-01-11 20:46:23 +0800

    [webflux] dual blocking exception.

--- msa-webflux/src/test/java/com/chenluo/service/MyServiceTest.java
+++ msa-webflux/src/test/java/com/chenluo/service/MyServiceTest.java
@@ -1,24 +1,47 @@
 package com.chenluo.service;
 
 import org.junit.jupiter.api.Test;
-
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
+
+import java.util.concurrent.ExecutionException;
 
 class MyServiceTest {
 
     @Test
-    void findById() {}
+    void findById() {
+    }
 
     @Test
-    void removeById() {}
+    void removeById() {
+    }
 
     @Test
-    void findAndRemove() {}
+    void findAndRemove() {
+    }
 
     @Test
     void testReactor() {
         Mono<String> stringMono = Mono.just("S");
         StepVerifier.create(stringMono).expectNext("S").expectComplete().verify();
+    }
+
+    @Test
+    void testWebclient() throws ExecutionException, InterruptedException {
+        WebClient webClient = WebClient.create();
+        //        Mono<String> stringMono = webClient.get().uri("https://www.google.com").retrieve()
+        //                .bodyToMono(new ParameterizedTypeReference<String>() {
+        //                }).doOnNext(s -> System.out.println(s));
+        //
+        //        String block = stringMono.block();
+        //        System.out.println(block);
+
+        webClient.post().uri("https://www.google.com").exchange()
+                .publishOn(Schedulers.boundedElastic()).doOnSuccess(cr -> {
+                    System.out.println("doOnSucess");
+                    System.out.println(cr.bodyToMono(String.class).block());
+                }).block();
     }
 }
