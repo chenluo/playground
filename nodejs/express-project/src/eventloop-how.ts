8f7c3950b0b2a121f5adcadac24034946e0cd1b1
chenluo <chenluo.cn@gmail.com>
2024-07-18 00:11:24 +0800

    [nodejs] event loop investigation

---
+++ nodejs/express-project/src/eventloop-how.ts
@@ -0,0 +1,13 @@
+
+for (let i = 0; i < 16; i++) {
+    setTimeout(() => {
+        const id = i;
+        while (1) {
+            console.log(id);
+            const start = new Date
+            while (new Date().getTime() < start.getTime()+1000) {
+                
+            }
+        }
+    })
+}
