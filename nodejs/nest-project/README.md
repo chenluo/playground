a3cd0e2ab150b7338fa0804395ecb7123a198550
chenluo <chenluo.cn@gmail.com>
2024-11-23 00:01:00 +0800

    serverless integration

--- nodejs/nest-project/README.md
+++ nodejs/nest-project/README.md
@@ -97,3 +97,10 @@ Nest is an MIT-licensed open source project. It can grow thanks to the sponsors 
 ## License
 
 Nest is [MIT licensed](https://github.com/nestjs/nest/blob/master/LICENSE).
+
+
+## serverless invocation
+
+visit http://localhost:3000/dev/[endpoint]
+
+example: http://localhost:3000/dev/connect
--- nodejs/nest-project/src/main.ts
+++ nodejs/nest-project/src/main.ts
@@ -3,27 +3,27 @@ import serverlessExpress from '@codegenie/serverless-express';
 import { AppModule } from './app.module';
 import { Handler, Context, Callback } from 'aws-lambda';
 
-// let server: Handler;
+let server: Handler;
 
-// async function bootstrap(): Promise<Handler> {
-//   const app = await NestFactory.create(AppModule);
-//   await app.init();
-
-//   const expressApp = app.getHttpAdapter().getInstance();
-//   return serverlessExpress({ app: expressApp });
-// }
-
-// export const handler: Handler = async (
-//   event: any,
-//   context: Context,
-//   callback: Callback,
-// ) => {
-//   server = server ?? (await bootstrap());
-//   return server(event, context, callback);
-// };
-
-async function bootstrap() {
+async function bootstrap(): Promise<Handler> {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.init();
+
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: expressApp });
 }
-bootstrap();
+
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  server = server ?? (await bootstrap());
+  return server(event, context, callback);
+};
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(process.env.PORT ?? 3000);
+// }
+// bootstrap();
